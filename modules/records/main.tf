4df33c8d55f3d61b17e014caf502362b7363b79e
Antonio Cuenco <antonio.cuenco@rackspace.com>
2021-10-30 16:32:21 +1100

    Added Records module

---
+++ modules/records/main.tf
@@ -0,0 +1,68 @@
+locals {
+  records = try(jsondecode(var.records), var.records)
+  recordsets = {
+    for rs in local.records :
+    join(" ", compact(["${rs.name} ${rs.type}", lookup(rs, "set_identifier", "")])) => merge(rs, {
+      records = jsonencode(try(rs.records, null))
+    })
+  }
+}
+
+data "aws_route53_zone" "this" {
+  count = var.create && (var.zone_id != null || var.zone_name != null) ? 1 : 0
+
+  zone_id      = var.zone_id
+  name         = var.zone_name
+  private_zone = var.private_zone
+}
+
+resource "aws_route53_record" "this" {
+  for_each = var.create && (var.zone_id != null || var.zone_name != null) ? local.recordsets : tomap({})
+
+  zone_id = data.aws_route53_zone.this[0].zone_id
+
+  name                             = each.value.name != "" ? "${each.value.name}.${data.aws_route53_zone.this[0].name}" : data.aws_route53_zone.this[0].name
+  type                             = each.value.type
+  ttl                              = lookup(each.value, "ttl", null)
+  records                          = jsondecode(each.value.records)
+  set_identifier                   = lookup(each.value, "set_identifier", null)
+  health_check_id                  = lookup(each.value, "health_check_id", null)
+  multivalue_answer_routing_policy = lookup(each.value, "multivalue_answer_routing_policy", null)
+  allow_overwrite                  = lookup(each.value, "allow_overwrite", false)
+
+  dynamic "alias" {
+    for_each = length(keys(lookup(each.value, "alias", {}))) == 0 ? [] : [true]
+
+    content {
+      name                   = each.value.alias.name
+      zone_id                = try(each.value.alias.zone_id, data.aws_route53_zone.this[0].zone_id)
+      evaluate_target_health = lookup(each.value.alias, "evaluate_target_health", false)
+    }
+  }
+
+  dynamic "failover_routing_policy" {
+    for_each = length(keys(lookup(each.value, "failover_routing_policy", {}))) == 0 ? [] : [true]
+
+    content {
+      type = each.value.failover_routing_policy.type
+    }
+  }
+
+  dynamic "weighted_routing_policy" {
+    for_each = length(keys(lookup(each.value, "weighted_routing_policy", {}))) == 0 ? [] : [true]
+
+    content {
+      weight = each.value.weighted_routing_policy.weight
+    }
+  }
+
+  dynamic "geolocation_routing_policy" {
+    for_each = length(keys(lookup(each.value, "geolocation_routing_policy", {}))) == 0 ? [] : [true]
+
+    content {
+      continent   = lookup(each.value.geolocation_routing_policy, "continent", null)
+      country     = lookup(each.value.geolocation_routing_policy, "country", null)
+      subdivision = lookup(each.value.geolocation_routing_policy, "subdivision", null)
+    }
+  }
+}
